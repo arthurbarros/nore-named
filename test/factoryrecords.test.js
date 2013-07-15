9d66e7c2423bfe315c21d683911cbedd693214a5
Arthur Barros <arthbarros@gmail.com>
2013-07-15 16:52:57 -0300

    refactoring

--- test/factoryrecords.test.js
+++ test/factoryrecords.test.js
@@ -1,6 +1,7 @@
 var assert = require('assert'), 
     FactoryRecords = require('../lib/FactoryRecords.js'),
     ARecord = require('../node_modules/node-named/lib/records/a.js'),
+    SOARecord = require('../node_modules/node-named/lib/records/soa.js'),
     path = require('path');
 
 describe('FactoryRecords', function(){
@@ -10,11 +11,11 @@ describe('FactoryRecords', function(){
             var factory = new FactoryRecords();
             var aRecord = new ARecord('8.8.8.8');
             var buildObject = { 
-            name: 'google.com',
+                name: 'google.com',
+                type: 'A',
+                ttl: 400,
                 properties: {
-                    type: 'A',
-                    ttl: 400,
-                    resolve_to: '8.8.8.8'
+                    resolution: '8.8.8.8'
                 }
             };
 
@@ -35,6 +36,44 @@ describe('FactoryRecords', function(){
                 factory.path('a'), 
                 path.join(__dirname, '../node_modules/node-named/lib/records/a')
             );
+        });
+    });
+});
+
+describe('FactoryRecords', function(){
+    describe('#configuration()', function(){
+        it('should return SOARecord valid instance', function(){
+            var factory = new FactoryRecords();
+            var buildObject = { 
+                name: 'google.com',
+                type: 'SOA',
+                ttl: 400,
+                properties: {
+                    serial: '8.8.8.8'
+                }
+            };
+            var result = factory.configuration(buildObject);
+            var soaRecord = new SOARecord('google.com',{serial: '8.8.8.8'});
+            assert.deepEqual(result , soaRecord);
+        });
+    });
+});
+
+describe('FactoryRecords', function(){
+    describe('#configuration()', function(){
+        it('should return ARecord valid instance', function(){
+            var factory = new FactoryRecords();
+            var buildObject = { 
+                name: 'google.com',
+                type: 'A',
+                ttl: 400,
+                properties: {
+                    resolution: '8.8.8.8'
+                }
+            };
+            var result = factory.configuration(buildObject);
+            var aRecord = new ARecord('8.8.8.8');
+            assert.deepEqual(result , aRecord);
         });
     });
 });
