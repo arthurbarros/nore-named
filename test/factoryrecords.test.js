1e414531e7d4af87f8b45f9d42110334fa59eda7
Arthur Barros <arthbarros@gmail.com>
2013-07-17 14:09:16 -0300

    tests

--- test/factoryrecords.test.js
+++ test/factoryrecords.test.js
@@ -1,7 +1,12 @@
 var assert = require('assert'), 
     FactoryRecords = require('../lib/FactoryRecords.js'),
     ARecord = require('../node_modules/node-named/lib/records/a.js'),
+    AAAARecord = require('../node_modules/node-named/lib/records/aaaa.js'),
     SOARecord = require('../node_modules/node-named/lib/records/soa.js'),
+    TXTRecord = require('../node_modules/node-named/lib/records/txt.js'),
+    CNAMERecord = require('../node_modules/node-named/lib/records/cname.js'),
+    MXRecord = require('../node_modules/node-named/lib/records/mx.js'),
+    SRVRecord = require('../node_modules/node-named/lib/records/srv.js'),
     path = require('path');
 
 describe('FactoryRecords', function(){
@@ -74,6 +79,104 @@ describe('FactoryRecords', function(){
             var result = factory.configuration(buildObject);
             var aRecord = new ARecord('8.8.8.8');
             assert.deepEqual(result , aRecord);
+        });
+    });
+});
+
+describe('FactoryRecords', function(){
+    describe('#configuration()', function(){
+        it('should return TXTRecord valid instance', function(){
+            var factory = new FactoryRecords();
+            var buildObject = { 
+                name: 'google.com',
+                type: 'TXT',
+                ttl: 400,
+                properties: {
+                    txt: 'google-uid-8946431631'
+                }
+            };
+            var result = factory.configuration(buildObject);
+            var txtRecord = new TXTRecord('google-uid-8946431631');
+            assert.deepEqual(result , txtRecord);
+        });
+    });
+});
+
+describe('FactoryRecords', function(){
+    describe('#configuration()', function(){
+        it('should return CNAMERecord valid instance', function(){
+            var factory = new FactoryRecords();
+            var buildObject = { 
+                name: 'google.com',
+                type: 'CNAME',
+                ttl: 400,
+                properties: {
+                    cname: 'srv-01.appengine.google.com'
+                }
+            };
+            var result = factory.configuration(buildObject);
+            var cnameRecord = new CNAMERecord('srv-01.appengine.google.com');
+            assert.deepEqual(result , cnameRecord);
+        });
+    });
+});
+
+describe('FactoryRecords', function(){
+    describe('#configuration()', function(){
+        it('should return AAAARecord valid instance', function(){
+            var factory = new FactoryRecords();
+            var buildObject = { 
+                name: 'google.com',
+                type: 'AAAA',
+                ttl: 400,
+                properties: {
+                    resolution: '8.8.8.8'
+                }
+            };
+            var result = factory.configuration(buildObject);
+            var aaaaRecord = new AAAARecord('8.8.8.8');
+            assert.deepEqual(result , aaaaRecord);
+        });
+    });
+});
+
+describe('FactoryRecords', function(){
+    describe('#configuration()', function(){
+        it('should return MXRecord valid instance', function(){
+            var factory = new FactoryRecords();
+            var buildObject = { 
+                name: 'google.com',
+                type: 'MX',
+                properties: {
+                    priority: 0,
+                    ttl: 600,
+                    exchange: 'mail.google.com'
+                }
+            };
+            var result = factory.configuration(buildObject);
+            var mxRecord = new MXRecord('mail.google.com', {priority: 0,ttl: 600});
+            assert.deepEqual(result , mxRecord);
+        });
+    });
+});
+
+describe('FactoryRecords', function(){
+    describe('#configuration()', function(){
+        it('should return MXRecord valid instance', function(){
+            var factory = new FactoryRecords();
+            var buildObject = { 
+                name: 'google.com',
+                type: 'SRV',
+                properties: {
+                    priority: 5,
+                    weight: 10,
+                    port: 8864,
+                    resolution:'8.8.8.8'
+                }
+            };
+            var result = factory.configuration(buildObject);
+            var srvRecord = new MXRecord('8.8.8.8', 8864, {priority: 0, weight: 10});
+            assert.deepEqual(result , srvRecord);
         });
     });
 });
